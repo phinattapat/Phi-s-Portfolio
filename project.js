c7b74a365d22a01929381440ad4aa4b0fd5bc889
Nattapat Nipatjakaranan <nattapat.ni@icloud.com>
2024-08-18 11:50:08 +0700

    Huge Update  - Add project page  - Make data file  - Add shadows  - Fix header and footer  - More decorations  - Use javascript for loading data from another file  - Add project picture  - Fixed bug

---
+++ project.js
@@ -0,0 +1,18 @@
+const project = [
+    {
+        
+        title : "Portfolio Website",
+        description : "This is a portfolio website I've created from scratch. And it took many weeks to make this website.",
+        languageUsed : ["HTML","Javascrript","CSS"],
+        image : "./images/web.png"
+        
+    },
+    {
+       
+        title : "Calculator",
+        description : "Description: A basic calculator created with Python's Tkinter library. It features a clean grid layout with buttons for numbers, basic arithmetic operations, and functions like Back, AC, and percentage. The design is simple and user-friendly, perfect for quick calculations.",
+        languageUsed : ["Python"],
+        image : "./images/calculator.png"
+        
+    }
+]
